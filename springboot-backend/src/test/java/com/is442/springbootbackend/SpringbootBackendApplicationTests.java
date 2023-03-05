327aec7d7e068fcaf6ed37c5959439f20334f809
Jing Yi Yeo <89132913+yjyyy17@users.noreply.github.com>
2023-03-05 21:39:45 +0800

    springboot files

---
+++ springboot-backend/src/test/java/com/is442/springbootbackend/SpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is442.springbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
